df1fb73c55304f15e369cc39b598054c84f8c739
Phillip Wagner <m0ssyblue7@gmail.com>
2025-12-09 12:13:37 -0800

    bug with opening file, working on fix

--- main.cpp
+++ main.cpp
@@ -9,16 +9,24 @@ int main () {
 
     std::map<std::string, int> traffic;
 
-    std::ifstream in("210-final-1-FA25.txt");
+    std::ifstream in("final.codes.txt");
     if (!in) {
         cout << "Error opening file." << endl;
         return 1;
     }
 
     std::string origin;
-    std;;sting destination;
+    std::string destination;
 
-    while
+    while (in >> origin >> destination) {
+        traffic[origin]++;
+        traffic[destination]++;
+    }
+
+    std::cout << "All airport traffic counts:\n";
+    for (const auto &entry : traffic) {
+        std::cout << entry.first << " " << entry.second << '\n';
+    }
 
     return 0;
 }
