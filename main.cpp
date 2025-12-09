32428846979293e82a1eff5f002bd66383cff3b1
Phillip Wagner <m0ssyblue7@gmail.com>
2025-12-09 12:03:06 -0800

    Working on milestone 1

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main () {
+
+    std::map<std::string, int> traffic;
+
+    std::ifstream in("210-final-1-FA25.txt");
+    if (!in) {
+        cout << "Error opening file." << endl;
+        return 1;
+    }
+
+    std::string origin;
+    std;;sting destination;
+
+    while
+
+    return 0;
+}
