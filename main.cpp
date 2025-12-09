4499601851c0a5eb36e6bc5dbe5f9c4472646f1c
Phillip Wagner <m0ssyblue7@gmail.com>
2025-12-09 13:11:18 -0800

    Milestone 4

--- main.cpp
+++ main.cpp
@@ -8,11 +8,16 @@ using namespace std;
 
 void printAirportsInRange(const std::map<std::string, int> &traffic, int low, int high) {
     std::cout << "\nAirports with traffix in range [" << low << ", " << high << "]:\n";
+    std::cout << std::left << std::setw(10) << "Airport" << " | "
+              << std::right << std::setw(7)  << "Traffic" << "\n";
+
     for (const auto &entry : traffic) {
         if (entry.second >= low && entry.second <= high) {
-            std::cout << entry.first << " " << entry.second << '\n';
+            std::cout << std::left << std::setw(10) << entry.first << " | "
+                      << std::right << std::setw(7)  << entry.second << "\n";
         }
     }
+    std::cout << "\n";
 }
 
 int main () {
